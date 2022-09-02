a0cf328419ee42b7b79e3c4843923452258cc365
Hyper-Programmer <hyperprogrammeri@gmail.com>
2022-09-02 15:07:35 +0530

    Added LocationService and Firebase

--- NWKRTC-Driver/app/src/main/java/com/adinathanikam/nwkrtc_driver/MainActivity.java
+++ NWKRTC-Driver/app/src/main/java/com/adinathanikam/nwkrtc_driver/MainActivity.java
@@ -1,19 +1,34 @@
 package com.adinathanikam.nwkrtc_driver;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.Looper;
-import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,137 +37,230 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    // initializing
-    // FusedLocationProviderClient
-    // object
-    FusedLocationProviderClient mFusedLocationClient;
+    private static int MY_FINE_LOCATION_REQUEST = 99;
+    private static int MY_BACKGROUND_LOCATION_REQUEST = 100;
 
-    // Initializing other items
-    // from layout file
-    TextView TV_res;
-    int PERMISSION_ID = 44;
+
+
+
+
+    LocationService mLocationService = new LocationService();
+    Intent mServiceIntent;
+
+    Button startServiceBtn, stopServiceBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TV_res = findViewById(R.id.TV_CUR_LOC);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        startServiceBtn = findViewById(R.id.startService);
+        stopServiceBtn = findViewById(R.id.stopService);
 
-        // method to get the location
-        getLastLocation();
-    }
+        /**************/
 
-    @SuppressLint("MissingPermission")
-    private void getLastLocation() {
-        // check if permissions are given
-        if (checkPermissions()) {
+        startServiceBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-            // check if location is enabled
-            if (isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
 
-                // getting last
-                // location from
-                // FusedLocationClient
-                // object
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        Location location = task.getResult();
-                        if (location == null) {
-                            requestNewLocationData();
-                        } else {
-                            TV_res.setText(location.getLatitude() + "\n" + location.getLongitude());
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+
+
+                            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                            alertDialog.setTitle("Background permission");
+                            alertDialog.setMessage(getString(R.string.background_location_permission_message));
+
+                            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Start service anyway",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            starServiceFunc();
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Grant background Permission",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestBackgroundLocationPermission();
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                            alertDialog.show();
+
+
+                        }else if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                                == PackageManager.PERMISSION_GRANTED){
+                            starServiceFunc();
                         }
+                    }else{
+                        starServiceFunc();
                     }
-                });
-            } else {
-                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
+
+                }else if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                        alertDialog.setTitle("ACCESS_FINE_LOCATION");
+                        alertDialog.setMessage("Location permission required");
+
+                        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Ok",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestFineLocationPermission();
+                                        dialog.dismiss();
+                                    }
+                                });
+
+
+                        alertDialog.show();
+
+                    } else {
+                        requestFineLocationPermission();
+                    }
+                }
+
             }
-        } else {
-            // if permissions aren't available,
-            // request for permissions
-            requestPermissions();
-        }
+        });
+
+        stopServiceBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopServiceFunc();
+            }
+        });
+
+
+        /***********/
+
     }
 
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData() {
-
-        // Initializing LocationRequest
-        // object with appropriate methods
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(5);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        // setting LocationRequest
-        // on FusedLocationClient
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        ((FusedLocationProviderClient) mFusedLocationClient).requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private LocationCallback mLocationCallback = new LocationCallback() {
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            TV_res.setText(mLastLocation.getLatitude() + "" + mLastLocation.getLongitude());
-        }
-    };
-
-    // method to check for permissions
-    private boolean checkPermissions() {
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-
-        // If we want background location
-        // on Android 10.0 and higher,
-        // use:
-        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    // method to request for permissions
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-
-    // method to check
-    // if location is enabled
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    // If everything is alright then
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
-    public void
-    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
+        Toast.makeText(this, Integer.toString(requestCode), Toast.LENGTH_LONG).show();
+
+        if ( requestCode == MY_FINE_LOCATION_REQUEST){
+
+            if (grantResults.length !=0 /*grantResults.isNotEmpty()*/ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    requestBackgroundLocationPermission();
+                }
+
+            } else {
+                Toast.makeText(this, "ACCESS_FINE_LOCATION permission denied", Toast.LENGTH_LONG).show();
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                 /*   startActivity(
+                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", this.getPackageName(), null),),);*/
+
+                    startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:com.adinathanikam.nwkrtc_driver;")
+                    ));
+
+                }
             }
+            return;
+
+        }else if (requestCode == MY_BACKGROUND_LOCATION_REQUEST){
+
+            if (grantResults.length!=0 /*grantResults.isNotEmpty()*/ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Background location Permission Granted", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Background location permission denied", Toast.LENGTH_LONG).show();
+            }
+            return;
+        }
+
+    }
+
+    private void starServiceFunc(){
+        mLocationService = new LocationService();
+        mServiceIntent = new Intent(this, mLocationService.getClass());
+        if (!Util.isMyServiceRunning(mLocationService.getClass(), this)) {
+            startService(mServiceIntent);
+            Toast.makeText(this, getString(R.string.service_start_successfully), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.service_already_running), Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (checkPermissions()) {
-            getLastLocation();
+    private void stopServiceFunc(){
+        mLocationService = new LocationService();
+        mServiceIntent = new Intent(this, mLocationService.getClass());
+        if (Util.isMyServiceRunning(mLocationService.getClass(), this)) {
+            stopService(mServiceIntent);
+            Toast.makeText(this, "Service stopped!!", Toast.LENGTH_SHORT).show();
+            FirebaseDatabase.getInstance().getReference().child("NWKRTC-Driver").removeValue();
+            //saveLocation(); // explore it by your self
+        } else {
+            Toast.makeText(this, "Service is already stopped!!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void requestBackgroundLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                MY_BACKGROUND_LOCATION_REQUEST);
+    }
+
+    private void requestFineLocationPermission() {
+        ActivityCompat.requestPermissions(this,  new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, MY_FINE_LOCATION_REQUEST);
+    }
+
+    public void saveLocation(){
+        File dir = new File(this.getFilesDir(), "trickyworld");
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+
+        try {
+            File userLocation = new File(dir, "userlocation.txt");
+            FileWriter writer = new FileWriter(userLocation);
+            writer.append(LocationService.locationArrayList.toString());
+            writer.flush();
+            writer.close();
+            LocationService.locationArrayList.clear();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
